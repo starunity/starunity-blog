d7a2c4f463500874dd42214c452a563c7e9223a7
starunity <star@starunity.dev>
2022-07-24 18:35:09 +0800

    feat: 🎸 add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://blog.starunity.dev',
+  integrations: [tailwind(), sitemap()],
 })
