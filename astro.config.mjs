af89f0bfcc605a42be77a6a9abe0282a8ae82aff
starunity <star@starunity.dev>
2022-07-25 05:33:08 +0000

    feat: 🎸 add astro-robots-txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.starunity.dev',
-  integrations: [tailwind(), sitemap()],
+  experimental: {
+    integrations: true,
+  },
+  integrations: [tailwind(), sitemap(), robotsTxt()],
 })
