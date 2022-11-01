edd0afad959c2c903b12fd6c6d598f67c6058992
starunity <star@starunity.dev>
2022-11-01 07:38:05 +0000

    feat: 🎸 add astro-compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import sitemap from '@astrojs/sitemap'
 import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
+import compress from 'astro-compress'
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://blog.starunity.dev',
   experimental: {
     integrations: true,
   },
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), robotsTxt(), compress()],
 })
